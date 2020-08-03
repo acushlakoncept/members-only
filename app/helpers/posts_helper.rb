5f24e4634ac67fd811660466528c6d12a89904f0
Uduak Essien <acushla4real@gmail.com>
2020-08-03 18:08:59 +0100

    add create_post_btn helper method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  
+    def create_post_btn
+      out = ''
+      if member_signed_in?
+        out << link_to('Create Post', new_post_path, class: 'float-right mr-2 btn btn-outline-secondary')
+      end
+      out.html_safe
+    end
 end
+
+
