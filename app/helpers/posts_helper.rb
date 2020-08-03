9b699547f35e2e9aef0ee9c233f4740827851e1c
Uduak Essien <acushla4real@gmail.com>
2020-08-03 18:27:54 +0100

    fix rubocop lint error

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,20 +1,15 @@
 module PostsHelper
-  
-    def create_post_btn
-      out = ''
-      if member_signed_in?
-        out << link_to('Create Post', new_post_path, class: 'float-right mr-2 btn btn-outline-secondary')
-      end
-      out.html_safe
+  def create_post_btn
+    out = ''
+    if member_signed_in?
+      out << link_to('Create Post', new_post_path, class: 'float-right mr-2 btn btn-outline-secondary')
     end
+    out.html_safe
+  end
 
-    def show_author_name(post)
-        out = ''
-        if member_signed_in?
-            out << "<small><em>by #{post.member.name} </em></small>"
-        end
-        out.html_safe
-    end
+  def show_author_name(post)
+    out = ''
+    out << "<small><em>by #{post.member.name} </em></small>" if member_signed_in?
+    out.html_safe
+  end
 end
-
-
