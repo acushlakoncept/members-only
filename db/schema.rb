ce732e67cd32d1afa2e2c2862948676aeec8b043
Uduak Essien <acushla4real@gmail.com>
2020-08-03 10:50:27 +0100

    Add post model and did migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_094514) do
+ActiveRecord::Schema.define(version: 2020_08_03_094809) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +24,14 @@ ActiveRecord::Schema.define(version: 2020_08_03_094514) do
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "member_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_posts_on_member_id"
+  end
+
+  add_foreign_key "posts", "members"
 end
