2e4be3bac35565872215ac37e8a59dcc48d9a9a8
Uduak Essien <acushla4real@gmail.com>
2020-08-03 11:13:19 +0100

    add model associations

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,6 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
 end
