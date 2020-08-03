cb55eec5f4797700177f5838943e53a9ef253c88
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-03 11:34:07 -0700

    add routing resources for posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
+  resources :posts
 end
