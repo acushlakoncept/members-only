a46003039c91fbbd7f73386197cab346e5785c2b
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-03 14:22:28 -0700

    update posts view index to show every post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   # before_action :authenticate_member!, only: %i[new create]
 
     def index
+      @posts = Post.all
     end
     def new
         @post = Post.new
