3520b1a6318bb3fe7e258735ef7d583c01d030c3
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-03 12:13:31 -0700

    add new and create action in post controller and setting a before_action check

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
     def index
     end
+    def new
+        @post = current_user.post.build
+    end
+    def create
+        @post = current_user.post.build(posts_params)
+        if @post.save
+            flash[:notice] = 'Post was created successfully'
+            redirect_to posts_path
+        else
+            flash[:alert] = 'Woops something went wrong'
+            render 'new'
+        end
+    end
+    
+    private
+
+  def posts_params
+    params.require(:post).permit(:title, :body)
+  end
 end
