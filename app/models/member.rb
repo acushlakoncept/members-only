ecb78dc02a757680f7296ff50b20f88a9f99ae57
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-03 19:00:06 -0700

    add validation for member and post model

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,4 +5,9 @@ class Member < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+
+  validates :name, :password, presence: true, length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false },
+   format: { with: VALID_EMAIL_REGEX }
 end
